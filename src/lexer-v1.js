108a7aad17d0938d4cad251ca005843be0bd90de
code123841 <udhay1co.de@gmail.com>
2022-04-11 12:12:12 +0530

    LexerCode Test Passing

--- src/lexer-v1.js
+++ src/lexer-v1.js
@@ -1,10 +1,14 @@
-function lexerv1(){
-    return code.split(/\s+/)
-    .filter(function (t) {return t.length > 0})
-    .map(function (t) {
-        return isNaN(t)
-        ?{type: 'word' , value: t}
-        : {type: 'number' , value: t}
-    })
-}
-lexerv1();
+function lexer (code) {
+    var code = "Paper 100"
+    var lol =  code.split(/\s+/)
+            .filter(function (t) { return t.length > 0 })
+            .map(function (t) {
+
+              return isNaN(t)
+                      ? {type: 'word', value: t}
+                      : {type: 'number', value: t}
+            })
+            console.log(lol)
+            
+  }
+  lexer();
