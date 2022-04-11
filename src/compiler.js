6bd657af465ea98b8dd75f15a62428eaa2bf7963
code123841 <udhay1co.de@gmail.com>
2022-04-11 12:26:20 +0530

    :D

--- src/compiler.js
+++ src/compiler.js
@@ -2,16 +2,18 @@ import { lexer, parser } from './parser.js';
 import { transformer } from './transformer.js';
 import { generator } from './generator.js';
 
-var SBN = {}
+var sbn = {}
+sbn.VERSION = '0.0.1'
+sbn.lexer = lexer
+sbn.parser = parser
+sbn.transformer = transformer
+sbn.generator = generator
 
-SBN.VERSION = '0.5.6'
-SBN.lexer = lexer
-SBN.parser = parser
-SBN.transformer = transformer
-SBN.generator = generator
-
-SBN.compile = function (code) {
+sbn.compile = function (code) {
   return this.generator(this.transformer(this.parser(this.lexer(code))))
 }
 
-export default SBN
+// call sbn compiler
+var code = 'Paper 0 Pen 100 Line 0 50 100 50'
+var svg = sbn.compile(code)
+document.body.innerHTML = svg
