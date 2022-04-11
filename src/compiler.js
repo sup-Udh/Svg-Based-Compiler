478f6980a46a84c90fbea3eed7f540d744f2256a
code123841 <udhay1co.de@gmail.com>
2022-04-11 12:28:22 +0530

    triangle

--- src/compiler.js
+++ src/compiler.js
@@ -2,18 +2,18 @@ import { lexer, parser } from './parser.js';
 import { transformer } from './transformer.js';
 import { generator } from './generator.js';
 
-var sbn = {}
-sbn.VERSION = '0.0.1'
-sbn.lexer = lexer
-sbn.parser = parser
-sbn.transformer = transformer
-sbn.generator = generator
+var Svg= {}
+Svg.VERSION = '0.0.1'
+Svg.lexer = lexer
+Svg.parser = parser
+Svg.transformer = transformer
+Svg.generator = generator
 
-sbn.compile = function (code) {
+Svg.compile = function (code) {
   return this.generator(this.transformer(this.parser(this.lexer(code))))
 }
 
 // call sbn compiler
-var code = 'Paper 0 Pen 100 Line 0 50 100 50'
-var svg = sbn.compile(code)
+var code = 'Paper 100 Pen 0 Line 50 77 22 27 Line 22 27 78 27 Line 78 27 50 77'
+var svg = Svg.compile(code)
 document.body.innerHTML = svg
